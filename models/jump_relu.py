9347e6ff40b5ac0039b3330fb63dc1d680e500a8
jiito <benjamin.allanrahill@gmail.com>
2025-09-06 12:40:18 -0700

    basic impl of jumprelu

---
+++ models/jump_relu.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+from torch.types import Tensor
+
+# taken from: https://github.com/erichson/JumpReLU/blob/master/activationfun.py#L12
+
+
+class JumpReLU(nn.Module):
+    r"""
+    This is a module that implements the Jump Rectified Linear Unit function element-wise
+    """
+
+    def __init__(self, jump=0.0):
+        super().__init__()
+        self.jump = jump
+
+    def _threshold(arr: Tensor, threshold=0.0) -> Tensor:
+        arr[arr <= threshold] = 0.0
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._threshold(x, self.jump)
