9fdbe81b48d6311172cbe553faa2de3e5936b89f
jiito <benjamin.allanrahill@gmail.com>
2025-09-08 16:42:03 -0700

    test CLT

---
+++ tests/test_clt.py
@@ -0,0 +1,9 @@
+import torch
+from models.clt import CrossLayerTranscoder
+
+
+def test_clt():
+    clt = CrossLayerTranscoder(d_activations=12, d_features=10, n_layers=2)
+    x = torch.randn(100, 2, 12)
+    act = clt(x)
+    assert act.shape == (100, 2, 12)
